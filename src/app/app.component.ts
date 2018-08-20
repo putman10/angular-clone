be7e9a4d57a95ba9db9d387da30965b7674a94bc
Ryan Putman <putman10@me.com>
2018-08-20 04:19:19 -0700

    add article model with construtor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './models/article.model'
 
 @Component({
   selector: 'app-root',
---
+++ src/app/models/article.model.ts
@@ -0,0 +1,3 @@
+export class Article {
+  constructor(public title: string, public subtitle: string, public author: string, public image: string, public category: string, public publishedDate: Date)
+}
