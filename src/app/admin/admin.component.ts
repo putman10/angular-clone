d5e72822bec496d732447808c403b87c9763ed8e
Ryan Putman <putman10@me.com>
2018-08-21 22:59:31 -0700

    add admin firebase get data and loop to display with edit and delete buttons

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleService } from '../article.service';
+import { Article } from '../models/article.model';
+import { Router } from '@angular/router';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [ArticleService]
 })
 export class AdminComponent implements OnInit {
+  articles: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private router: Router, private articleService: ArticleService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
+    this.articles = this.articleService.getArticles();
   }
 
 }
