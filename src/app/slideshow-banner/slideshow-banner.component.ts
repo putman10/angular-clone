ba0f90e0ff9fd07c2271b03ccfc6b71b92b3d205
Ryan Putman <putman10@me.com>
2018-08-24 11:12:55 -0700

    add slideshow links to route to article

--- src/app/slideshow-banner/slideshow-banner.component.ts
+++ src/app/slideshow-banner/slideshow-banner.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-slideshow-banner',
@@ -8,10 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class SlideshowBannerComponent implements OnInit {
 
   imageUrls = [
-    { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56748793/dbohn_170625_1801_0018.0.0.jpg', caption: 'The first slide', href: '#config' },
-    { url: 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/9278671/jbareham_170917_2000_0124.jpg', clickAction: () => alert('custom click function') },
-    { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56789263/akrales_170919_1976_0104.0.jpg', caption: 'Apple TV', href: 'https://www.apple.com/' },
-    'https://cdn.vox-cdn.com/uploads/chorus_image/image/56674755/mr_pb_is_the_best.0.jpg'
+    { url: 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/9278671/jbareham_170917_2000_0124.jpg', clickAction: () => routerLink="admin", caption: 'Test Caption' }
   ];
   height: string = '400px';
   minHeight: string;
